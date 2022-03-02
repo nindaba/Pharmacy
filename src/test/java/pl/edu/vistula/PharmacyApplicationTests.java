aaebff9be01dd15e53e643f90452776c50c0db55
nindaba <arthurninda@gmail.com>
2022-03-02 23:30:10 +0100

    initial commit

---
+++ src/test/java/pl/edu/vistula/PharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
